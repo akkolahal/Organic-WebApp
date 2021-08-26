c25076dbe41ac942a97d8be5b918ef9582b03fd6
akkolahal <49974303+akkolahal@users.noreply.github.com>
2021-08-27 03:26:07 +0530

    Add files via upload

---
+++ product.ts
@@ -0,0 +1,11 @@
+export class Product{
+    id: number;
+    productname :string;
+    qty : number;
+
+    constructor(){
+        this.id=0;
+        this.productname="";
+        this.qty=0;
+    }
+}
